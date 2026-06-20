491b62f2523585be28eef4ebc6885595255a3e94
LENOVO\adami <adamikiskandar17@gmail.com>
2026-06-20 21:45:03 +0800

    update1

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import profileImage from "../assets/images/Portfolio_pfp.jpeg";
+import profileImage from "../assets/images/Portfolio_pfp2.png";
 import '../assets/styles/Main.scss';
 
 
